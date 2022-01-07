184ae93e72e834ae13383c30a0399139d248e9f5
LucPrestin <40767277+LucPrestin@users.noreply.github.com>
2022-01-07 12:59:08 +0100

    Differ testing (#8)
    * add first differ tests
    
    * add further tests
    
    * remove unneeded empty test file, fix spelling
    
    * add check to differ to detect non-morphs
    
    * remove unneeded test
    
    Co-authored-by: LucPrestin <LucPrestin@users.noreply.github.com>

--- lively.merger/differ.js
+++ lively.merger/differ.js
@@ -1,48 +1,50 @@
 import { Color } from 'lively.graphics';
+
 export class Differ {
   static diffMorphsWithIds (morph1id, morph2id) {
     const morph1 = $world.submorphs.filter(morph => morph.id === morph1id)[0];
     if (!morph1) {
-      $world.setStatusMessage('Cant diff morphs, morph1 not found');
-      return {};
+      throw new Error(`Cannot diff morphs, morph1 with id ${morph1id} not found`);
     }
 
     const morph2 = $world.submorphs.filter(morph => morph.id === morph2id)[0];
     if (!morph2) {
-      $world.setStatusMessage('Cant diff morphs, morph2 not found');
-      return {};
+      throw new Error(`Cannot diff morphs, morph2 with id ${morph2id} not found`);
     }
 
     return this.diffMorphs(morph1, morph2);
   }
 
   static diffMorphs (morph1, morph2) {
+    if (!morph1.styleClasses || !morph1.styleClasses.includes('morph') || !morph2.styleClasses || !morph2.styleClasses.includes('morph')) {
+      throw new Error('Cannot diff objects that are not morphs');
+    }
+
     if (JSON.stringify(morph1.styleClasses) != JSON.stringify(morph2.styleClasses)) {
-      $world.setStatusMessage('Cant diff morphs, classes differ');
-      return {};
+      throw new Error('Cannot diff morphs, styleclasses differ');
     }
 
     const firstMorphProperties = morph1.propertiesAndPropertySettings().properties;
     const secondMorphProperties = morph2.propertiesAndPropertySettings().properties;
 
     let matchingProperties = {};
-    let differentProperties = {};
+    let differingProperties = {};
 
     for (const [key, value] of Object.entries(firstMorphProperties)) {
       if (key == 'submorphs' && morph1[key].length > 0 && morph2[key].length > 0) {
-        console.log('Uh shit they have submorphs'); // TODO
+        // console.log('Uh shit they have submorphs'); // TODO
       }
 
-      if (JSON.stringify(morph1[key]) == JSON.stringify(morph2[key])) {
+      if (JSON.stringify(morph1[key]) === JSON.stringify(morph2[key])) {
         matchingProperties[key] = morph1[key];
       } else {
-        differentProperties[key] = [morph1[key], morph2[key]];
+        differingProperties[key] = [morph1[key], morph2[key]];
       }
     }
 
-    console.log('matching: ', matchingProperties);
-    console.log('different: ', differentProperties);
+    // console.log('matching: ', matchingProperties);
+    // console.log('different: ', differingProperties);
 
-    return { matchingProperties, differentProperties };
+    return { matchingProperties, differingProperties };
   }
 }
